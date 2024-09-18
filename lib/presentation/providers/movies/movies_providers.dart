519b0a86f1e1d7bacbbc7b8de10d579a70a9dfa9
Gelo Figueroa <gelofiguero@gmail.com>
2024-09-18 00:40:40 -0300

    nowPlay Provider and notifier

---
+++ lib/presentation/providers/movies/movies_providers.dart
@@ -0,0 +1,26 @@
+import 'package:cinemapedia/domain/entities/entities.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final nowPlayingMoviesProvider =
+    StateNotifierProvider<MoviesProviders, List<Movie>>((ref) {
+  final fetchMoreMovie = ref.watch(moviesRepositoryProvider).getNowPlaying();
+
+  return MoviesProviders(fetchMoreMovie: fetchMoreMovie);
+});
+
+typedef MovieCallBack = Future<List<Movie>> Function({int page});
+
+class MoviesProviders extends StateNotifier<List<Movie>> {
+  int currentPage = 0;
+  MovieCallBack fetchMoreMovie;
+
+  MoviesProviders({required this.fetchMoreMovie}) : super([]);
+
+  Future<void> loadNextPage() async {
+    currentPage++;
+
+    final List<Movie> movie = await fetchMoreMovie(page: currentPage);
+    state = [...state, ...movie];
+  }
+}
--- lib/presentation/providers/movies/movies_repository_provider.dart
+++ lib/presentation/providers/movies/movies_repository_provider.dart
@@ -2,6 +2,6 @@ import 'package:cinemapedia/infrastructure/datasources/moviedb_datasource.dart';
 import 'package:cinemapedia/infrastructure/repository/movie_repository_impl.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final movieRepositoryProvider = Provider((ref) {
+final moviesRepositoryProvider = Provider((ref) {
   return MovieRepositoryImpl(datasource: MoviedbDatasource());
 });
