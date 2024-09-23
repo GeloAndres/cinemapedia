089c5f16b10640058200339a31a42abcbd9b7f3b
Gelo Figueroa <gelofiguero@gmail.com>
2024-09-23 09:42:32 -0300

    instalacion intl & formateo de numeros de popularidad

--- lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -1,4 +1,5 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/config/helpers/human_formats.dart';
 import 'package:cinemapedia/domain/entities/entities.dart';
 import 'package:flutter/material.dart';
 
@@ -88,25 +89,29 @@ class _Slider extends StatelessWidget {
           ),
 
           //*Rating
-          Row(
-            children: [
-              Icon(
-                Icons.star_half_outlined,
-                color: Colors.yellow.shade800,
-              ),
-              const SizedBox(
-                width: 3,
-              ),
-              Text(
-                '${movie.voteAverage.truncateToDouble()}',
-                style: textStyle.bodyMedium
-                    ?.copyWith(color: Colors.yellow.shade800),
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              Text('${movie.popularity}')
-            ],
+          SizedBox(
+            width: 150,
+            child: Row(
+              children: [
+                Icon(
+                  Icons.star_half_outlined,
+                  color: Colors.yellow.shade800,
+                ),
+                const SizedBox(
+                  width: 3,
+                ),
+                Text(
+                  '${movie.voteAverage.truncateToDouble()}',
+                  style: textStyle.bodyMedium
+                      ?.copyWith(color: Colors.yellow.shade800),
+                ),
+                const Spacer(),
+                Text(
+                  HumanFormats.number(movie.popularity),
+                  style: textStyle.bodySmall,
+                )
+              ],
+            ),
           )
         ],
       ),
