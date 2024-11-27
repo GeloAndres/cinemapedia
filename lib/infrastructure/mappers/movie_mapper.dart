9395d8835cb2fbf3bc3bf69f50dedd944e95ef8f
Gelo Figueroa <gelofiguero@gmail.com>
2024-11-27 14:51:07 -0300

    peliculas en el buscador y su detalles

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -16,8 +16,8 @@ class MovieMapper {
       popularity: moviedb.popularity,
       posterPath: (moviedb.posterPath != '')
           ? 'https://image.tmdb.org/t/p/w500/${moviedb.posterPath}'
-          : 'no-poster',
-      releaseDate: moviedb.releaseDate,
+          : 'https://img.freepik.com/vector-premium/minimo-moderno-pagina-error-404-sitio-web-error-404-pagina-no-encontrada-concepto-fantasma-muerto_599740-702.jpg?w=2000',
+      releaseDate: moviedb.releaseDate ?? DateTime.now(),
       title: moviedb.title,
       video: moviedb.video,
       voteAverage: moviedb.voteAverage,
@@ -27,7 +27,7 @@ class MovieMapper {
       adult: moviedb.adult,
       backdropPath: (moviedb.backdropPath != '')
           ? 'https://image.tmdb.org/t/p/w500/${moviedb.backdropPath}'
-          : 'https://img.freepik.com/free-vector/404-error-lost-space-concept-illustration_114360-7901.jpg?t=st=1725419144~exp=1725422744~hmac=429ff39d3aecdb7e310e560ceffbae1b4810528b268fe68514ead54688594313&w=1380',
+          : 'https://img.freepik.com/vector-premium/minimo-moderno-pagina-error-404-sitio-web-error-404-pagina-no-encontrada-concepto-fantasma-muerto_599740-702.jpg?w=2000',
       genreIds: moviedb.genres.map((e) => e.name).toList(),
       id: moviedb.id,
       originalLanguage: moviedb.originalLanguage,
@@ -36,7 +36,7 @@ class MovieMapper {
       popularity: moviedb.popularity,
       posterPath: (moviedb.posterPath != '')
           ? 'https://image.tmdb.org/t/p/w500/${moviedb.posterPath}'
-          : 'no-poster',
+          : 'https://static.wikia.nocookie.net/ideas/images/6/66/FoxAndroidTM2%27s_No_Poster.jpg/revision/latest?cb=20230213155127',
       releaseDate: moviedb.releaseDate,
       title: moviedb.title,
       video: moviedb.video,
