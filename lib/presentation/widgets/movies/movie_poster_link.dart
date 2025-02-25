8e01d1d7c95927325df7accf09185526d171d969
Gelo Figueroa <54201948+GeloAndres@users.noreply.github.com>
2025-02-25 00:31:11 -0300

    saltos lista popular

--- lib/presentation/widgets/movies/movie_poster_link.dart
+++ lib/presentation/widgets/movies/movie_poster_link.dart
@@ -9,12 +9,15 @@ class MoviePosterLink extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FadeInUp(
-      child: GestureDetector(
-        onTap: () => context.push('/home/0/movie/${movie.id}'),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(17),
-          child: Image.network(movie.posterPath),
+    return SizedBox(
+      height: 186,
+      child: FadeInUp(
+        child: GestureDetector(
+          onTap: () => context.push('/home/0/movie/${movie.id}'),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(17),
+            child: Image.network(movie.posterPath),
+          ),
         ),
       ),
     );
